26ecac5239b84f1883567cf1a3c70ddca5b931bd
Vanessa <bossglop43@gmail.com>
2026-06-04 06:47:46 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,7 @@ import Home from '@/pages/Home';
 import Messages from '@/pages/Messages';
 import Network from '@/pages/Network';
 import Files from '@/pages/Files';
-import Studio from '@/pages/Studio';
-import StudioEditor from '@/pages/StudioEditor';
+
 import Record from '@/pages/Record';
 import Settings from '@/pages/Settings';
 import Explore from '@/pages/Explore';
@@ -89,8 +88,7 @@ const AuthenticatedApp = () => {
           <Route path="/messages" element={<Messages />} />
           <Route path="/network" element={<Network />} />
           <Route path="/files" element={<Files />} />
-          <Route path="/studio" element={<Studio />} />
-          <Route path="/studio-editor" element={<StudioEditor />} />
+
           <Route path="/record" element={<Record />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/leaderboard" element={<Leaderboard />} />

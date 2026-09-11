c915d92edd7551637092919d13aae8b105936129
daddieboo82 <bossglop43@gmail.com>
2026-09-11 10:03:02 -0400

    Release both squad membership claims on leave

--- base44/functions/leaveSquad/entry.ts
+++ base44/functions/leaveSquad/entry.ts
@@ -14,10 +14,12 @@ Deno.serve(async (req) => {
 
     const entities = base44.asServiceRole.entities;
     await entities.Squad.update(squad.id, { status: 'ended' });
-    await entities.User.updateMany(
-      { id: user.id, squad_membership_id: squad.id },
-      { $set: { squad_membership_id: null } },
-    ).catch(() => {});
+    for (const memberId of [squad.member_a_id, squad.member_b_id].filter(Boolean)) {
+      await entities.User.updateMany(
+        { id: memberId, squad_membership_id: squad.id },
+        { $set: { squad_membership_id: null } },
+      ).catch(() => {});
+    }
     return Response.json({ success: true });
   } catch (error) {
     return Response.json({ error: error?.message || 'Could not leave squad' }, { status: 500 });

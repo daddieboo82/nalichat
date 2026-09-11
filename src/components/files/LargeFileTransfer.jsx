89e4b3dac33372f81b8fe9e9a396ff4fd05efb3a
daddieboo82 <bossglop43@gmail.com>
2026-09-11 02:12:40 -0400

    Point file shares to public tokenized download route

--- src/components/files/LargeFileTransfer.jsx
+++ src/components/files/LargeFileTransfer.jsx
@@ -65,7 +65,7 @@ function LargeFileTransferContent({ currentUser }) {
       const share = await base44.functions.invoke("createFileShareLink", { fileId: newFile.id });
       const token = share?.data?.token;
       if (!token) throw new Error("Could not create share link");
-      const link = `${window.location.origin}/files?download=${encodeURIComponent(newFile.id)}&token=${encodeURIComponent(token)}`;
+      const link = `${window.location.origin}/shared-file?id=${encodeURIComponent(newFile.id)}&token=${encodeURIComponent(token)}`;
       setShareLink(link);
 
       if (recipientEmail) {

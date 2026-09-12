316385eb823ece32b0838719965ad905e7b266ce
daddieboo82 <bossglop43@gmail.com>
2026-09-12 01:04:51 -0400

    Fix clean-checkout verification error in DesktopNav.jsx

--- src/components/navigation/DesktopNav.jsx
+++ src/components/navigation/DesktopNav.jsx
@@ -1,5 +1,4 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
 import NotificationBell from "@/components/notifications/NotificationBell";

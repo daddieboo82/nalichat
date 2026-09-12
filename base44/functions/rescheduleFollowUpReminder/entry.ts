d3827da2dacabd9e803e95361835891c6adc975c
daddieboo82 <bossglop43@gmail.com>
2026-09-11 21:34:41 -0400

    Require POST for rescheduleFollowUpReminder

--- base44/functions/rescheduleFollowUpReminder/entry.ts
+++ base44/functions/rescheduleFollowUpReminder/entry.ts
@@ -6,6 +6,9 @@ import {
 
 Deno.serve(async (req) => {
   try {
+    if (req.method !== 'POST') {
+      return Response.json({ error: 'Method not allowed' }, { status: 405 });
+    }
     const base44 = createClientFromRequest(req);
     const user = await base44.auth.me();
     const { reminder_id, remind_at } = await req.json();

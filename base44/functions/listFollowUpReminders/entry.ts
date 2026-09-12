10f810287d39fd5222d1a39f4b4c5f6745d4f87e
daddieboo82 <bossglop43@gmail.com>
2026-09-12 15:37:55 -0400

    Require POST for reminder listing

--- base44/functions/listFollowUpReminders/entry.ts
+++ base44/functions/listFollowUpReminders/entry.ts
@@ -6,6 +6,10 @@ import {
 
 Deno.serve(async (req) => {
   try {
+    if (req.method !== 'POST') {
+      return Response.json({ error: 'Method not allowed' }, { status: 405 });
+    }
+
     const base44 = createClientFromRequest(req);
     const user = await base44.auth.me().catch(() => null);
     const reminders = await listFollowUpReminders({

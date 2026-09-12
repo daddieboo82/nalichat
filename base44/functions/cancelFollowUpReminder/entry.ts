6f686b398e79a06aa84394302ead9375b503de47
daddieboo82 <bossglop43@gmail.com>
2026-09-12 00:42:22 -0400

    Handle unauthenticated reminder requests in entry.ts

--- base44/functions/cancelFollowUpReminder/entry.ts
+++ base44/functions/cancelFollowUpReminder/entry.ts
@@ -11,7 +11,7 @@ Deno.serve(async (req) => {
     }
 
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
+    const user = await base44.auth.me().catch(() => null);
     const { reminder_id } = await req.json();
     const reminder = await cancelFollowUpReminder({
       entities: base44.asServiceRole.entities,

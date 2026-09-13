6f7b6561991fae845698fdd3be5fa9852d4105cd
daddieboo82 <bossglop43@gmail.com>
2026-09-12 21:25:19 -0400

    Align AI and workflow SDK version

--- base44/functions/suggestTrackTags/entry.ts
+++ base44/functions/suggestTrackTags/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.44';
 import { requireEntitlement, preferredAiModel } from '../../shared/entitlementAccess.ts';
 import { consumeHourlyLimit } from '../../shared/rateLimit.ts';
 import { acquireTrackLifecycleLock, releaseTrackLifecycleLock } from '../../shared/trackLifecycleLock.ts';

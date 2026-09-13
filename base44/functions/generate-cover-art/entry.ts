70a0ce4ac1e52d1bff76188f79671b6ecb28be95
daddieboo82 <bossglop43@gmail.com>
2026-09-12 21:25:12 -0400

    Align AI and workflow SDK version

--- base44/functions/generate-cover-art/entry.ts
+++ base44/functions/generate-cover-art/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.44';
 import { requireEntitlement } from '../../shared/entitlementAccess.ts';
 import { consumeHourlyLimit } from '../../shared/rateLimit.ts';
 import { isTrustedStoredMediaUrl } from '../../shared/mediaSecurity.ts';

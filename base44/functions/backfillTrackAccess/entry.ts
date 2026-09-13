27b8ad640fd2d26d214cb0b3a30a425db0308c7d
daddieboo82 <bossglop43@gmail.com>
2026-09-12 21:29:34 -0400

    Align admin workflow SDK version

--- base44/functions/backfillTrackAccess/entry.ts
+++ base44/functions/backfillTrackAccess/entry.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.31';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.44';
 import { consumeHourlyLimit } from '../../shared/rateLimit.ts';
 
 const PAGE_SIZE = 200;
